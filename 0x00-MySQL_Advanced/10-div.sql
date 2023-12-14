9d6b61e69f55e4191b3984b4ddede3212d49bbe7
proyirga <infoyirga12@gmail.com>
2023-12-14 17:19:42 +0300

    10. Safe divide correction

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -6,16 +6,12 @@
 --b, INT
 -- And returns a / b or 0 if b == 0
 
-DROP FUNCTION IF EXISTS SafeDiv;
-DELIMITER $$
-CREATE FUNCTION SafeDiv (a INT, b INT)
-RETURNS FLOAT DETERMINISTIC
+CREATE OR REPLACE FUNCTION SafeDiv(a INT, b INT)
+RETURNS INT
 BEGIN
-    DECLARE result FLOAT DEFAULT 0;
-
-    IF b != 0 THEN
-        SET result = a / b;
-    END IF;
-    RETURN result;
-END $$
-DELIMITER ;
+  IF b = 0 THEN
+    RETURN 0;
+  ELSE
+    RETURN a / b;
+  END IF;
+END;
